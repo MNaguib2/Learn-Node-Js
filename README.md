2380208a65448cbeb45b4a6bddb697933d83b666
mena afefe fawze <73434035+MNaguib2@users.noreply.github.com>
2021-06-11 23:49:38 +0200

    template Engnies
    (pug and ejs and express- handlebars)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Learn-Node-Js
 this Project to examples all steps to learn node JS
+
+npm init
+npm install --save express ejs body-parser
+npm install --save-dev nodemon
+npm install --save express-handlebars pug
---
+++ app.js
@@ -0,0 +1,35 @@
+const expree = require('express');
+
+const app = expree();
+
+const expressHbs = require('express-handlebars');
+
+const bodyParser = require('body-parser'); 
+
+const users = [];
+
+//app.engine('hbs', expressHbs({defaultLayout: 'main-layout', extname: 'hbs'}));
+/*
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+//*/
+/*
+app.set('view engine', 'pug');
+app.set('views', 'views');
+//*/
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', (req, res, next) => {
+    res.render('index', {pageTitle: "Add User"});
+})
+app.get('/users', (req, res, next) => {
+    res.render('users', {pageTitle: "User", users : users, hasUser : users.length > 0});
+})
+app.post('/add-user', (req, res, next) => {
+    users.push({name: req.body.username});
+    res.redirect('/users');
+})
+
+app.listen(3000);
